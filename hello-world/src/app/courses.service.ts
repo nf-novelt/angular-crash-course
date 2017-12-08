674c2977e4acd41463ec304634a7b8b38578ecf8
nfargere <nf@novel-t.ch>
2017-12-08 10:21:48 +0100

    Created CoursesService

---
+++ hello-world/src/app/courses.service.ts
@@ -0,0 +1,7 @@
+
+export class CoursesService {
+    getCourses() {
+        //Place here the logic to init an HTTP request to get the courses
+        return ["course1", "course2", "course2"];
+    }
+}
