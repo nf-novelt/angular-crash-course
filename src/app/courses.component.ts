e7a039e7b79f23e9f5a8f2e91dd62c428548fffe
nfargere <nf@novel-t.ch>
2017-12-08 14:54:17 +0100

    Interpolation & property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -6,6 +6,8 @@ import { CoursesService } from './courses.service';
         selector: 'courses',
         template: `
             <h2>{{ title }}</h2>
+            <img src="{{ imageUrl }}" width="200px" height="112px" />
+            <img [src]="imageUrl2" width="200px" height="112px" />
             <ul>
                 <li *ngFor="let course of courses">
                     {{ course }}
@@ -17,6 +19,8 @@ import { CoursesService } from './courses.service';
 export class CoursesComponent {
     title = "List of courses";
     courses;
+    imageUrl = "https://s-media-cache-ak0.pinimg.com/originals/4e/b1/96/4eb1965fe984f62e8f7734ff140f0604.jpg";
+    imageUrl2 = "https://images.adsttc.com/media/images/593f/dce6/e58e/ce96/bd00/0496/large_jpg/Singapore_Skyline_at_Night_with_Blue_Sky.jpg?1497357533";
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
