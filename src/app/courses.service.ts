d3dc4afbdcbbdd29059d6ae3c1035c9679e5c9d5
nfargere <nf@novel-t.ch>
2017-12-12 09:55:56 +0100

    ngIf final new school way

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -2,7 +2,7 @@
 export class CoursesService {
     getCourses() {
         //Place here the logic to init an HTTP request to get the courses
-        return ["course1", "course2", "course2"];
-        //return [];
+        //return ["course1", "course2", "course2"];
+        return [];
     }
 }
