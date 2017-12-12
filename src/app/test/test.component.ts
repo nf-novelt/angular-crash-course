8bdeb596f7334ca6ed794bfb40478abe7aa06cf4
nfargere <nf@novel-t.ch>
2017-12-12 14:55:46 +0100

    ngStyle

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
   text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempor dapibus tempor. Pellentesque ullamcorper a neque quis egestas. Vivamus non felis in lectus fringilla interdum eu at mauris";
   viewMode = 'map';
+  canSave = true;
   
   constructor() { }
 
