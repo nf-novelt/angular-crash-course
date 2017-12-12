31c8bf327a82a5789ff448554655615a58903d21
nfargere <nf@novel-t.ch>
2017-12-12 13:33:52 +0100

    NgFor directove display index

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -3,6 +3,10 @@ export class CoursesService {
     getCourses() {
         //Place here the logic to init an HTTP request to get the courses
         //return ["course1", "course2", "course2"];
-        return [];
+        return [
+            { id: 1, name:"Course 1"},
+            { id: 2, name:"Course 2"},
+            { id: 3, name:"Course 3"}
+        ];
     }
 }
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 )
 export class CoursesComponent {
     title = "List of courses";
-    courses: string[];
+    courses: {id: number, name: string}[] = [];
     imageUrl = "https://s-media-cache-ak0.pinimg.com/originals/4e/b1/96/4eb1965fe984f62e8f7734ff140f0604.jpg";
     imageUrl2 = "https://images.adsttc.com/media/images/593f/dce6/e58e/ce96/bd00/0496/large_jpg/Singapore_Skyline_at_Night_with_Blue_Sky.jpg?1497357533";
 
@@ -19,6 +19,6 @@ export class CoursesComponent {
     }
 
     onClick_addCourse(newCourse: string) {
-        this.courses.push(newCourse);
+        this.courses.push({id: 999, name: newCourse});
     }
 }
