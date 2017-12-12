b69946c66ea1edc63e615f6c816b250a9fd30558
nfargere <nf@novel-t.ch>
2017-12-12 15:32:26 +0100

    Added onFocus and onBlur methods in the directive

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-
   constructor() { }
+  
+  @HostListener('focus') onFocus() {
+    console.log("on Focus");
+  }
+
+  @HostListener('blur') onBlur() {
+    console.log("on Blur");
+  }
 
 }
