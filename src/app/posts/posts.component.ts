078b700d718b3ec661a554fe314543379b5ce73c
nfargere <nf@novel-t.ch>
2017-12-15 18:59:36 +0100

    Handle unexpected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
+import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-posts',
@@ -7,10 +7,9 @@ import { Http } from '@angular/http';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts:any[];
-  private url = 'https://jsonplaceholder.typicode.com/posts';
+  posts:any[];  
 
-  constructor(private http: Http) {
+  constructor(private service: PostService) {
     
   }
 
@@ -18,7 +17,7 @@ export class PostsComponent implements OnInit {
     let post = { title: input.value }
     input.value = '';
 
-    this.http.post(this.url, JSON.stringify(post))
+    this.service.createPost(post)
       .subscribe(response => {
         post['id'] = response.json().id;
         
@@ -26,28 +25,40 @@ export class PostsComponent implements OnInit {
         //To add it at the end, use push()
         this.posts.splice(0, 0, post);
         console.log(response.json());
+      }, error => {
+        alert('An unexpected error occured');
+        console.log(error);
       })
   }
   
   updatePost(post) {    
-    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+    this.service.updatePost(post)
     .subscribe(response => {      
       console.log(response.json());
+    }, error => {
+      alert('An unexpected error occured');
+      console.log(error);
     })
   }
 
   deletePost(post) {
-    this.http.delete(this.url + '/' + post.id)
+    this.service.deletePost(post.id)
       .subscribe(response => {
         let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
+      }, error => {
+        alert('An unexpected error occured');
+        console.log(error);
       })
   }
 
   ngOnInit() {
-    this.http.get(this.url)
+    this.service.getPosts()
     .subscribe(response => {
       this.posts = response.json();
+    }, error => {
+      alert('An unexpected error occured');
+      console.log(error);
     })
   }
 
